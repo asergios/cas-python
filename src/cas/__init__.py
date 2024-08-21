ea1d8a32c644f0e43b674048c494c2412fb26427
António Silva <asergiosilv@gmail.com>
2024-08-21 19:14:59 +0100

    Fix path to JSON database

--- src/cas/__init__.py
+++ src/cas/__init__.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 import cas.db
 
+BASE_DIR = Path(__file__).resolve().parent
+
 # Define the path to the JSON database
-DATABASE_PATH = Path("cas/database/database.json")
+DATABASE_PATH = BASE_DIR / "database" / "database.json"
 
 # Create an instance of CASDatabase using the specified path
 database = cas.db.CASDatabase(database_path=DATABASE_PATH)
